0ec46bbf9e1787fc0b648aea5166452275604155
gokulkrh <gokulkrh@gmail.com>
2023-04-03 18:17:41 +0530

    fetch comments from api

---
+++ Fetch_Comments/fetch_comments.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+import os
+import requests
+
+
+def get_comment_corpus(videoid, num_comments):
+    text_data = []
+    comment_corpus = ""
+    load_dotenv()
+    api_key = os.getenv("API_key")
+    url = "https://youtube.googleapis.com/youtube/v3/commentThreads?videoId=" + videoid
+    params = {'key': api_key, 'Accept': "application/json", 'order': 'relevance', 'part': "snippet"}
+
+    while len(text_data) <= num_comments:
+        r = requests.get(url=url, params=params)
+        data = r.json()
+        for i in data["items"]:
+            comm = i["snippet"]["topLevelComment"]["snippet"]["textDisplay"]
+            comment = comm.lower()
+            text_data.append(comment)
+
+        if data.get("nextPageToken"):
+            params.update({"pageToken": data["nextPageToken"]})
+
+    for i in text_data:
+        comment_corpus += i
+        comment_corpus += "\n"
+
+    return comment_corpus
+
+
+if __name__ == "__main__":
+    print(get_comment_corpus("5QiW4kOxXVg", 10))
