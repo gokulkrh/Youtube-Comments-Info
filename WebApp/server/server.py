75156f1623d94e7ba78afd4e356c692bb5af4028
gokulkrh <gokulkrh@gmail.com>
2023-04-05 00:19:11 +0530

    defined api endpoints

---
+++ WebApp/server/server.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+import mock_responses
+
+app = Flask(__name__)
+
+
+@app.route("/fyp/v1/emo_stats/<videoid>")
+def return_emo_stats(videoid):
+    return jsonify(mock_responses.mock_emotion)
+
+
+@app.route("/fyp/v1/named_entities/<videoid>")
+def return_named_entities(videoid):
+    return jsonify(mock_responses.mock_named_entities)
+
+
+@app.route("/fyp/v1/comments_summary/<videoid>")
+def return_comments_summary(videoid):
+    return jsonify(mock_responses.mock_summary)
+
+
+@app.route("/fyp/v1/all/<videoid>")
+def return_everything(videoid):
+    return jsonify(mock_responses.mock_all)
+
+
+if __name__=="__main__":
+    app.run(debug=True)
