d401e5471b8bbbe6a04cf7f445ae778a3ce6fd83
gokulkrh <gokulkrh@gmail.com>
2023-04-19 23:17:03 +0530

    sandbox check

--- WebApp/server/server.py
+++ WebApp/server/server.py
@@ -1,27 +1,36 @@
 from flask import Flask, jsonify
 import mock_responses
+import os
 
 app = Flask(__name__)
 
 
 @app.route("/fyp/v1/emo_stats/<videoid>")
 def return_emo_stats(videoid):
-    return jsonify(mock_responses.mock_emotion)
+    if os.getenv("Sandbox"):
+        return jsonify(mock_responses.mock_emotion)
+    
 
 
 @app.route("/fyp/v1/named_entities/<videoid>")
 def return_named_keywords(videoid):
-    return jsonify(mock_responses.mock_named_keywords)
+    if os.getenv("Sandbox"):
+        return jsonify(mock_responses.mock_named_keywords)
+
 
 
 @app.route("/fyp/v1/comments_summary/<videoid>")
 def return_comments_summary(videoid):
-    return jsonify(mock_responses.mock_summary)
+    if os.getenv("Sandbox"):
+        return jsonify(mock_responses.mock_summary)
+
 
 
 @app.route("/fyp/v1/all/<videoid>")
 def return_everything(videoid):
-    return jsonify(mock_responses.mock_all)
+    if os.getenv("Sandbox"):
+        return jsonify(mock_responses.mock_all)
+
 
 
 if __name__=="__main__":
